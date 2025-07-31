b7c28740dbdb8a5182cf3c06f3218d9bb9b15978
Matthias <me@motz.xyz>
2025-07-31 16:46:21 +0200

    Dockerize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-export default function Home() {
+import { redis } from "bun"
+
+export const dynamic = "force-dynamic"
+
+export default async function Home() {
+    const bingo = await redis.get("bingo")
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-8"></main>
+        <main className="flex min-h-screen flex-col items-center justify-center p-8">
+            Hello from Sviss Todo <br /> Running on {Bun.version}
+            <p>Bingo: {bingo}</p>
+        </main>
     )
 }
